ee4abe76d9f2caad0bafe2f4a018734819f0e08e
Taakami <94414245+Taakami@users.noreply.github.com>
2025-05-24 22:32:11 +0200

    14th

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -3,13 +3,15 @@
   programs.waybar = {
     enable = true;
     settings = {
-      layer = "top";
-      position = "top";
-      modules-left = [ "sway/workspaces" ];
-      modules-center = [ "sway/window" ];
-      modules-right = [ "battery" "clock" "backlight" "network" ];
-      clock = {
-        timezone = "Europe/Paris";
+        mainBar = {
+        layer = "top";
+        position = "top";
+        modules-left = [ "sway/workspaces" ];
+        modules-center = [ "sway/window" ];
+        modules-right = [ "battery" "clock" "backlight" "network" ];
+        clock = {
+          timezone = "Europe/Paris";
+        };
       };
     };
   };
