08261f4ab19a895ea973996c9f86069c6afebb8e
Taakami <94414245+Taakami@users.noreply.github.com>
2025-05-25 10:42:14 +0200

    24th

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -30,10 +30,7 @@
         inner = 8;
         outer = 8;
       };
-      keybindings = 
-        let
-        modifier = config.wayland.windowManager.sway.config.modifier;
-        in lib.mkOptionDefault {
+      keybindings = lib.mkOptionDefault {
         # Switch to workspace
         "${modifier}+ampersand" = "workspace number 1";
         "${modifier}+eacute" = "workspace number 2";
@@ -45,16 +42,16 @@
         "${modifier}+underscore" = "workspace number 8";
         "${modifier}+ccedilla" = "workspace number 9";
         "${modifier}+agrave" = "workspace number 10";
-        "${modifier}+Shift+ampersand" = "move container to workspace number 1"
-        "${modifier}+Shift+eacute" = "move container to workspace number 2"
-        "${modifier}+Shift+quotedbl" = "move container to workspace number 3"
-        "${modifier}+Shift+apostrophe" = "move container to workspace number 4"
-        "${modifier}+Shift+parenleft" = "move container to workspace number 5"
-        "${modifier}+Shift+minus" = "move container to workspace number 6"
-        "${modifier}+Shift+egrave" = "move container to workspace number 7"
-        "${modifier}+Shift+underscore" = "move container to workspace number 8"
-        "${modifier}+Shift+ccedilla" = "move container to workspace number 9"
-        "${modifier}+Shift+agrave" = "move container to workspace number 10"
+        "${modifier}+Shift+ampersand" = "move container to workspace number 1";
+        "${modifier}+Shift+eacute" = "move container to workspace number 2";
+        "${modifier}+Shift+quotedbl" = "move container to workspace number 3";
+        "${modifier}+Shift+apostrophe" = "move container to workspace number 4";
+        "${modifier}+Shift+parenleft" = "move container to workspace number 5";
+        "${modifier}+Shift+minus" = "move container to workspace number 6";
+        "${modifier}+Shift+egrave" = "move container to workspace number 7";
+        "${modifier}+Shift+underscore" = "move container to workspace number 8";
+        "${modifier}+Shift+ccedilla" = "move container to workspace number 9";
+        "${modifier}+Shift+agrave" = "move container to workspace number 10";
       };
     };
     extraConfig = ''
