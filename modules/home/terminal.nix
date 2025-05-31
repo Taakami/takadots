0a68bffb946227ebed60f5fc8d3198ad892b868d
Taakami <94414245+Taakami@users.noreply.github.com>
2025-05-31 11:27:16 +0200

    40th

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -5,10 +5,28 @@
         enable = true;
         enableGitIntegration = true;
         font = {
-            name = "JetBrainsMono";
+            name = "JetBrains Mono";
             size = 16;
         };
     };
-    alacritty.enable = true;
+    alacritty = {
+      enable = true;
+      settings = {
+        window = {
+          opacity = 0.7;
+          blur = true;
+          padding = {
+            x = 8;
+            y = 8;
+          };
+        };
+        font = {
+          normal = {
+            family = "JetBrains Mono";
+            size = 16;
+          };
+        }
+      };
+    };
   };
 }
