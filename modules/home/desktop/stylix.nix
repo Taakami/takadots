dea422e21c22d3211f62b1c8080e77c2a3bfc625
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-06 23:26:44 +0200

    112th

--- modules/home/desktop/stylix.nix
+++ modules/home/desktop/stylix.nix
@@ -33,15 +33,16 @@
       size = 32;
     };
     fonts = {
-    serif = config.stylix.fonts.monospace;
-    sansSerif = config.stylix.fonts.monospace;
-    monospace = {
-      package = pkgs.jetbrains-mono;
-      name = "JetBrains Mono";
-    };
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
+      serif = config.stylix.fonts.monospace;
+      sansSerif = config.stylix.fonts.monospace;
+      monospace = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
     };
     targets = {
       gtk.enable = true;
