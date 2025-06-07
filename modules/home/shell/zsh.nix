dd508e9a04518ddc68d577c4c55afde1ebeee615
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-07 22:39:52 +0200

    118th

--- modules/home/shell/zsh.nix
+++ modules/home/shell/zsh.nix
@@ -6,7 +6,8 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     shellAliases = {
-      nix-switch = "nh os switch";
+      nix-switch = "nh os switch --accept-flake-config";
+      nix-switcho = "nh os switch";
       nix-update = "nh os switch --update";
       nix-clean = "nh clean all --keep 5";
     };
