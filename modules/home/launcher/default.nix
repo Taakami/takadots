7ae10a69f305aac65eca0dad81924fb6a3ebbdb9
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-18 14:26:20 +0200

    223th

--- modules/home/launcher/default.nix
+++ modules/home/launcher/default.nix
@@ -1,8 +1,7 @@
 { ... }:
 {
   imports = [
-    ./rofi.nix         # Rofi application launcher
-    # ./sherlock.nix     # Sherlock application launcher
-    # ./walker.nix       # Walker application launcher
+    ./rofi.nix         # Rofi
+    # ./sherlock.nix     # Sherlock
   ];
 }
