d83c196511b49107e0a367b712070a0c6de0124c
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-20 16:03:59 +0200

    246th

--- modules/home/desktop/wallpaper.nix
+++ modules/home/desktop/wallpaper.nix
@@ -1,27 +1,6 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ waypaper ];
-  # xdg.configFile."waypaper/config.ini".text = ''
-  #   [Settings]
-  #   language = fr
-  #   folder = ~/Pictures/wallpapers/
-  #   wallpaper = ~/Pictures/wallpapers/wallpaper.jpg
-  #   monitors = All
-  #   backend = swww
-  #   fill = fill
-  #   sort = name
-  #   color = #ffffff
-  #   subfolders = True
-  #   show_hidden = False
-  #   show_gifs_only = false
-  #   number_of_columns = 3
-  #   swww_transition_type = any
-  #   swww_transition_step = 90
-  #   swww_transition_angle = 0
-  #   swww_transition_duration = 2
-  #   swww_transition_fps = 60
-  #   use_xdg_state = True
-  # '';
+  home.packages = with pkgs; [ waytrogen ];
   services = {
     swww.enable = true;
   };
