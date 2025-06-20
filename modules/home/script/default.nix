fbea8cbab5d2c569dffa9d999db0bb67bb138df8
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-20 15:24:41 +0200

    242th

--- modules/home/script/default.nix
+++ modules/home/script/default.nix
@@ -1,6 +1,26 @@
-{pkgs, ...}:
+{ pkgs, ... }:
+let
+  scriptDir = ./scripts;
+  scriptEntries = builtins.readDir scriptDir;
+
+  regularFiles = builtins.filter (name: scriptEntries.${name} == "regular") (
+    builtins.attrNames scriptEntries
+  );
+
+  shellScripts = builtins.filter (
+    name: builtins.match ".*\\.nu$" name != null
+  ) regularFiles;
+
+  mkScript = name: {
+    name = name;
+    value = pkgs.writeScriptBin (builtins.replaceStrings [ ".nu" ] [ "" ] name) (
+      builtins.readFile (scriptDir + "/${name}")
+    );
+  };
+
+  scriptsSet = builtins.listToAttrs (map mkScript shellScripts);
+  scripts = builtins.attrValues scriptsSet;
+in
 {
-  home.packages = [ 
-    pkgs.writeScriptBin "ima-artvee" (builtins.readFile ./scripts/scrap_artvee.nu)
-  ];
+  home.packages = scripts;
 }
