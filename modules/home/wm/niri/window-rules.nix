3f96da468ab28e21651fbb7e994f37ccd3ef6ca0
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-20 15:16:46 +0200

    239thh

--- modules/home/wm/niri/window-rules.nix
+++ modules/home/wm/niri/window-rules.nix
@@ -2,10 +2,10 @@
   programs.niri.settings.window-rules = [
     {
       geometry-corner-radius = {
-        bottom-left = 4;
-        bottom-right = 4;
-        top-left = 4;
-        top-right = 4;
+        bottom-left = 4.;
+        bottom-right = 4.;
+        top-left = 4.;
+        top-right = 4.;
       };
       clip-to-geometry = true;
     }
