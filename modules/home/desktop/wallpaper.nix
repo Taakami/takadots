26547a0aa88de61a26742be23bc10f0f96c294eb
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-20 16:21:28 +0200

    248th

--- modules/home/desktop/wallpaper.nix
+++ modules/home/desktop/wallpaper.nix
@@ -1,6 +1,6 @@
 { inputs, pkgs, ... }:
 {
-  home.packages = with pkgs; [ inputs.waytrogen.packages.${system}.default; ];
+  home.packages = with pkgs; [ inputs.waytrogen.packages.${system}.default ];
   services = {
     swww.enable = true;
   };
