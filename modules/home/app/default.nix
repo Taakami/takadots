e2d73e2d0072a970f1d61642795fd2107c4b5abd
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-20 15:56:24 +0200

    245th

--- modules/home/app/default.nix
+++ modules/home/app/default.nix
@@ -4,7 +4,7 @@
     ./discord.nix           # Discord client
     ./mpv.nix               # Video player
     ./obsidian.nix          # Obsidian
-    ./qutebrowser.nix       # qutebrowser
+    # ./qutebrowser.nix       # qutebrowser
     ./thunderbird.nix       # Thunderbird email client
     ./zathura.nix           # PDF viewer
     ./zen-browser.nix       # Zen browser
