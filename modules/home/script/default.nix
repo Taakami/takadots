3192bc9e547c1eead31dbcbf763038ee72bb3f1e
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-20 15:20:14 +0200

    241th

--- modules/home/script/default.nix
+++ modules/home/script/default.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}:
-let
-  scrap_artvee = pkgs.writeScript "ima-artvee" (builtins.readFile ./scripts/scrap_artvee.nu);
-in
 {
-  home.packages = [ scrap_artvee ];
+  home.packages = [ 
+    pkgs.writeScriptBin "ima-artvee" (builtins.readFile ./scripts/scrap_artvee.nu)
+  ];
 }
