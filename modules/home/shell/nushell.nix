3b063727fd71553ef5b69bbc87a9ed9301cf5453
Taakami <94414245+Taakami@users.noreply.github.com>
2025-06-21 14:21:41 +0200

    251th

--- modules/home/shell/nushell.nix
+++ modules/home/shell/nushell.nix
@@ -10,5 +10,10 @@
       query
       highlight
     ];
+    shellAliases = {
+      nix-switch = "nh os switch";
+      nix-update = "nh os switch --update";
+      nix-clean = "nh clean all --keep 5";
+    }
   };
 }
